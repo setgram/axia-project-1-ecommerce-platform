95b4fe620e7d2330bf9cb4ba81c14dee5776efdf
Wisdom Okogho <wisdommokoghoo@gmail.com>
2025-03-16 23:37:37 +0100

    .

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -8,11 +8,11 @@ export const verifyUser = async (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decoded.id).select("-password"); // Get user from DB
+    req.user = await User.findById(decoded.id).select("-password");
 
     if (!req.user) return res.status(401).json({ message: "User not found" });
 
-    next(); // Move to the next function
+    next();
   } catch (error) {
     res.status(400).json({ message: "Invalid token" });
   }
